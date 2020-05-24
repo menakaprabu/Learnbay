7d0951d5c80e790e9d31ef3b01ac08dfcad208eb
menaka <menakaprabu.13@gmail.com>
2020-05-24 15:28:43 -0700

    Count of the Unique pair of two sum which will sum to the given target.

--- src/main/java/Class3/Array/UniquePairOfTwoSum.java
+++ src/main/java/Class3/Array/UniquePairOfTwoSum.java
@@ -1,24 +1,25 @@
 package Class3.Array;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 //This class is to return the count of unique pair such that when we add them we get the target value.
 //Time complexity is O(n
 public class UniquePairOfTwoSum {
 
-    private static int getPair(int[] input, int target){
+    private static Map getPair(int[] input, int target){
         Arrays.sort(input);
         int fromFirst = 0;
         int fromLast = input.length -1;
 
-        int count = 0;
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        //2,5,6,7,8,11,15
+        int count = 1;
+        Map<Integer, List<Integer>> uniquePairMap = new HashMap<>();
+        //2,5,6,7,8,11,15 (2,11: 5,8: 6,7)
         while(fromFirst < fromLast){
             if(input[fromFirst] + input[fromLast] == target){
+               List<Integer> pairList = new ArrayList();
+               pairList.add(input[fromFirst]);
+               pairList.add(input[fromLast]);
+               uniquePairMap.put(count, pairList);
                count++;
                fromFirst++;
             }
@@ -28,12 +29,13 @@ public class UniquePairOfTwoSum {
                 fromFirst++;
             }
         }
-        return count;
+        return uniquePairMap;
     }
 
     public static void main(String[] args){
-        int[] input = new int[]{6, 12, 5, 7, 11, 8, 15};
-        int noOfPairs = getPair(input, 13);
-        System.out.println("no of pairs = "+noOfPairs);
+        int[] input = new int[]{6, 2, 5, 7, 11, 8, 15};
+        Map pairsMap = getPair(input, 13);
+        System.out.println("no of pairs = "+pairsMap.size());
+        System.out.println("uniquePairs = "+pairsMap);
     }
 }
